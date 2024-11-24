ca60cca2fa3e4eb845faa360d2162c70a0119943
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-11-24 15:52:55 +0300

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""UTF-8 Validation Module"""
+
+
+def validUTF8(data):
+    """
+    A function checks whether if the data is UTF-8 encoded or not
+
+    :param data: The data to be checked
+
+    :return: True if the data is UTF-8 encoded  False otherwise
+    """
+    binary_representation = []
+    flag = 0
+    valid = True
+    for i in data:
+        binary_representation.append(format(i, '08b'))
+    print(binary_representation)
+    for byte in range(len(binary_representation)):
+        if binary_representation[byte][0] == '0':
+            continue
+        elif binary_representation[byte][0:2] == '10' and flag > 0:
+            flag -= 1
+        elif binary_representation[byte][0:3] == '110':
+            if byte + 1 < len(binary_representation):
+                if binary_representation[byte + 1][0:2] == '10':
+                    flag = 1
+                else:
+                    valid = False
+            else:
+                valid = False
+        elif binary_representation[byte][0:4] == '1110':
+            if byte + 2 < len(binary_representation):
+                print(byte)
+                if (binary_representation[byte + 1][0:2] == '10'
+                        and binary_representation[byte + 2][0:2] == '10'):
+                    flag = 2
+                else:
+                    valid = False
+            else:
+                valid = False
+        elif binary_representation[byte][0:5] == '11110':
+            if byte + 3 < len(binary_representation):
+                if (binary_representation[byte + 1][0:2] == '10'
+                        and binary_representation[byte + 2][0:2] == '10'
+                        and binary_representation[byte + 3][0:2] == '10'):
+                    flag = 3
+                else:
+                    valid = False
+            else:
+                valid = False
+        else:
+            valid = False
+    return valid
