fd8f6205b30d1e1e3780087ada5e71a126bdcb57
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-11-28 18:13:56 +0300

    Task 0 improvements

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,24 +12,22 @@ def makeChange(coins, total):
             -1 if the total can not be met by the coins,
             number of coins that make up the total
     """
-    solution = {}
+
     if total <= 0:
         return 0
 
-    while total != 0:
-        passed_coins = [x for x in coins if total >= x]
-        if len(passed_coins) > 0:
-            coin = max(passed_coins)
-            if total // coin > 1:
-                if total % coin == 0:
-                    solution[coin] = total / coin
-                    return solution
-                else:
-                    solution[coin] = total // coin
-                    total -= coin * (total // coin)
-            else:
-                solution[coin] = 1
-                total -= coin
-        else:
-            return -1
-    return sum(solution.values())
+    coins.sort(reverse=True)
+
+    count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+
+        num_coins = total // coin
+        count += num_coins
+        total -= num_coins * coin
+
+    if total > 0:
+        return -1
+
+    return count
